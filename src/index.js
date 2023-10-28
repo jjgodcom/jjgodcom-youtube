15a3cb1825accf5cdc7275e2f60a3c028912750e
jjgodcom <jjgodcom1@gamil.com>
2023-10-28 16:35:08 +0900

    SCSS 셋팅하기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './assets/scss/style.scss'; // 모든 scss 연결하는 부분입니다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
