954b7e1cdc980ab75b56da91b9e6e27458b4a6c1
jjgodcom <jjgodcom1@gamil.com>
2023-10-28 17:34:41 +0900

    섹션 컴포넌트 구조화

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+// pages
 import Home from './pages/Home'
 import Port from './pages/Port'
 import Not from './pages/Not'
 
+// components
+import Header from './components/section/Header'
+import Main from './components/section/Main'
+import Footer from './components/section/Footer'
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/port' element={<Port />}></Route>
-        <Route path='*' element={<Not />}></Route>
-      </Routes>
+      <Header></Header>
+      <Main>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/port' element={<Port />}></Route>
+          <Route path='*' element={<Not />}></Route>
+        </Routes>
+      </Main>
+      <Footer></Footer>
     </BrowserRouter>
   )
 }
