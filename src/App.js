8025ba281bba2f4938d11cc17fe50bee1a9dd90c
jjgodcom <jjgodcom1@gamil.com>
2023-10-28 17:14:13 +0900

    페이지 세팅하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Port from './pages/Port'
+import Not from './pages/Not'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/port' element={<Port />}></Route>
+        <Route path='*' element={<Not />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
